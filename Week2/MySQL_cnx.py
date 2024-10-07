a27f4173086d82de575b68f81446fadf1b0a7553
WilliamTRev <william.terry@revature.com>
2024-10-07 14:10:51 -0400

    added python connection

--- Week2/MySQL_cnx.py
+++ Week2/MySQL_cnx.py
@@ -2,26 +2,30 @@
 #   pip install mysql-connector-python
 import mysql.connector
 
-cnx = mysql.connector.connect(user='root', password='password1',
+cnx = mysql.connector.connect(user='root', password='p4ssword',
                               host='127.0.0.1',
-                              database='myfirstdb')
+                              database='oct7')
 
 cursor = cnx.cursor()
 
-# Creating and using a new database
-# cursor.execute("CREATE DATABASE test_creation;")
-# cursor.execute("USE test_creation")
+# test on Persons table
+add_person = "INSERT INTO PersonS (lastname, FirstName, Age) VALUES ('Gretsky', 'Wayne', 60); "
+cursor.execute(add_person)
 
-# Inserting data into persons table
-# add_player = "INSERT INTO PerSonS (PersonID, lastname, FirstName, City, Sport) VALUES (10, 'Gretsky', 'Wayne', 'St. Louis', 'Hockey'); "
-# cursor.execute(add_player)
+# Creating and using a new database
+cursor.execute("DROP DATABASE IF EXISTS test_creation;")
+cursor.execute("CREATE DATABASE test_creation;")
+cursor.execute("USE test_creation")
+
+#Inserting data into persons table
+
 
 # Create tables
-# add_table_sport = "CREATE TABLE sports (SportID int, Sport varchar(255));"
-# cursor.execute(add_table_sport)
+add_table_sport = "CREATE TABLE sports (SportID int, Sport varchar(255));"
+cursor.execute(add_table_sport)
 
-# insert_sports = "INSERT INTO sports VALUES (1, 'Basketball'), (2, 'Football'), (3, 'Hockey');"
-# cursor.execute(insert_sports)
+insert_sports = "INSERT INTO sports VALUES (1, 'Basketball'), (2, 'Football'), (3, 'Hockey');"
+cursor.execute(insert_sports)
 
 # Query values
 # cursor.execute("SELECT lastname, persons.sport, sportid FROM persons JOIN sports ON persons.sport=sports.sport;")
@@ -34,19 +38,17 @@ while True:
     personID = int(input("Enter player ID: "))
     firstname = input("Enter player first name: ")
     lastname = input("Enter player last name: ")
-    city = input("Enter city: ")
-    sport = input("Enter sport: ")
+    age = input("Enter Age: ")
     # INSERT INTO persons VALUES (11, 'firstname', 'lastname', 'city', 'sport');
-    query = "INSERT INTO persons VALUES (" + str(personID) + ", '"+ firstname +"', '" + lastname + "', '" + city + "', '" + sport + "');"
-
+    query = "INSERT INTO persons VALUES (" + str(personID) + ", '"+ firstname +"', '" + lastname + "', '" + age + "');"
+    cursor.execute("USE oct7")
     cursor.execute(query)
 
-    cursor.execute("SELECT * FROM persons;")
-    for row in cursor:
-        print(row)
 
-# for row in cursor:
-#     print(row)
+cursor.execute("USE oct7")
+cursor.execute("SELECT * FROM persons;")
+for row in cursor:
+     print(row)
 
 cnx.commit()
 
